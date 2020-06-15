44ce8820d80fb5065a9a038b9421163b6161c641
urvashi-code1255 <urvashianand0906@gmail.com>
2020-06-15 20:52:19 +0530

    days 3-7 added

---
+++ Day-3_Intro_to_Conditional_Statements.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n%2!=0)
+    cout<<"Weird\n";
+    else
+    {
+        if(n>=2 && n<=5 || n>20)
+        cout<<"Not Weird\n";
+        else if(n>=6 && n<=20)
+        cout<<"Weird\n";
+    }
+    return 0;
+}
+
